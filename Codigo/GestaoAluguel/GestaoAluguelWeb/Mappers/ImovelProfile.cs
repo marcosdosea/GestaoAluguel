3c7cc7cd2b8698b91c87a9fec310cb9871bdb2a3
Tata2906 <Itaba200@>
2025-08-20 23:52:31 -0300

    Caso de uso manter Imovel. Resolve #102

---
+++ Codigo/GestaoAluguel/GestaoAluguelWeb/Mappers/ImovelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using GestaoAluguelWeb.Models;
+
+namespace GestaoAluguelWeb.Mappers
+{
+    public class ImovelProfile : Profile
+    {
+        public ImovelProfile()
+        {
+            CreateMap<ImovelModel, Imovel>().ReverseMap();
+        }
+    }
+}
