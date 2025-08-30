d2616dc938e273ce5cce759d62d759471a5eafac
Amanda de Jesus Melo <60000299+amandexspeed@users.noreply.github.com>
2025-08-29 23:42:59 -0300

    Minha PR

---
+++ Codigo/GestaoAluguel/GestaoAluguelWeb/Controllers/LocacaoController.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using GestaoAluguelWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Service;
+
+namespace GestaoAluguelWeb.Controllers
+{
+    public class LocacaoController : Controller
+    {
+
+        private readonly ILocacaoService LocacaoService;
+        private readonly IImovelService imovelService;
+        private readonly IMapper mapper;
+
+        public LocacaoController(ILocacaoService locacaoService, IImovelService imovelService, IMapper mapper)
+        {
+            this.LocacaoService = locacaoService;
+            this.imovelService = imovelService;
+            this.mapper = mapper;
+        }
+
+        // GET: LocacaoController
+        public IActionResult Index()
+        {
+            var locacoes = LocacaoService.GetAll(); // retorna List<Core.Locacao>
+            var locacoesModel = mapper.Map<List<LocacaoModel>>(locacoes); // mapeia para List<LocacaoModel>
+            return View(locacoesModel); // passa o model correto para a view
+        }
+
+        // GET: LocacaoController/Details/5
+        public ActionResult Details(int id)
+        {
+            var locacao = LocacaoService.Get(id);
+            LocacaoModel locacaoModel = mapper.Map<LocacaoModel>(locacao);
+            return View(locacaoModel);
+        }
+
+        // GET: LocacaoController/Create
+        public ActionResult Create(int? idImovel)
+        {
+            var locacaoModel = new LocacaoModel();
+
+            if (idImovel.HasValue)
+                locacaoModel.IdImovel = idImovel.Value; 
+
+            return View(locacaoModel);
+        }
+
+        public IActionResult VisualizarContrato(int id)
+        {
+            var locacao = LocacaoService.Get(id);
+            if (locacao == null || locacao.Contrato == null)
+            {
+                return NotFound();
+            }
+            return File(locacao.Contrato, "application/pdf");
+		}
+
+		// POST: LocacaoController/Create
+		[HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(LocacaoModel locacaoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var locacao = mapper.Map<Locacao>(locacaoModel);
+                LocacaoService.Create(locacao);
+
+                // Atualiza o imóvel para "alugado"
+                var imovel = imovelService.Get(locacaoModel.IdImovel);
+                if (imovel != null)
+                {
+                    imovel.EstaAlugado = 1;
+                    imovelService.Edit(imovel);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(locacaoModel);
+        }
+
+        // GET: LocacaoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Locacao? locacao = LocacaoService.Get(id);
+            LocacaoModel locacaoModel = mapper.Map<LocacaoModel>(locacao);
+
+            return View(locacaoModel);
+        }
+
+        // POST: LocacaoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(LocacaoModel locacaoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var locacao = mapper.Map<Locacao>(locacaoModel);
+                LocacaoService.Edit(locacao);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(locacaoModel);
+        }
+
+        // GET: LocacaoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var locacao = LocacaoService.Get(id);
+            LocacaoModel locacaoModel = mapper.Map<LocacaoModel>(locacao);
+            return View();
+        }
+
+        // POST: LocacaoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            LocacaoService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Codigo/GestaoAluguel/Service/LocacaoService.cs
@@ -0,0 +1,87 @@
+﻿using Core;
+using Core.DTO;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class LocacaoService : ILocacaoService
+    {
+        private readonly GestaoAluguelContext context;
+
+        public LocacaoService(GestaoAluguelContext context)
+        {
+            this.context = context;
+        }
+
+        public int Create(Locacao locacao)
+        {
+            context.Locacaos.Add(locacao);
+            context.SaveChanges();
+            return locacao.Id;
+        }
+
+        public void Delete(int id)
+        {
+            var locacao = context.Locacaos.Find(id);
+            if (locacao != null)
+            {
+                context.Locacaos.Remove(locacao);
+                context.SaveChanges();
+            }
+        }
+
+        public void Edit(Locacao locacao)
+        {
+            
+                context.Update(locacao);
+                context.SaveChanges();
+        }
+
+        public Locacao? Get(int id)
+        {
+            return context.Locacaos.Find(id);
+        }
+
+        public IEnumerable<Locacao> GetAll()
+        {
+            return context.Locacaos
+                .OrderBy(locacao => locacao.Id)
+                .ToList();
+        }
+
+        public IEnumerable<LocacaoDTO> GetByImovel(int idImovel)
+        {
+            return from locacao in context.Locacaos
+                   where locacao.IdImovel == idImovel
+                   select new LocacaoDTO
+                   {
+                       Id = locacao.Id,
+                       DataInicio = locacao.DataInicio,
+                       DataFim = locacao.DataFim,
+                       Status = locacao.Status,
+                       IdImovel = locacao.IdImovel,
+                       IdInquilino = locacao.IdInquilino
+                   };
+        }
+
+        public IEnumerable<LocacaoDTO> GetByInquilino(int idInquilino)
+        {
+            return from locacao in context.Locacaos
+                   where locacao.IdImovel == idInquilino
+                   select new LocacaoDTO
+                   {
+                       Id = locacao.Id,
+                       DataInicio = locacao.DataInicio,
+                       DataFim = locacao.DataFim,
+                       Status = locacao.Status,
+                       IdImovel = locacao.IdImovel,
+                       IdInquilino = locacao.IdInquilino
+                   };
+        }
+    }
+}
