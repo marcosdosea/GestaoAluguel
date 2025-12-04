9be8a52ee39993098040a19de716dbb730b26b85
Tata2906 <Itaba200@>
2025-12-04 20:10:27 -0300

    Foram feitas as alterações necessarias para concluir a tarefa e reorganizado o padrão de tela. Resolve #173

--- Codigo/GestaoAluguel/GestaoAluguelWeb/Mappers/LocacaoProfile.cs
+++ Codigo/GestaoAluguel/GestaoAluguelWeb/Mappers/LocacaoProfile.cs
@@ -6,9 +6,22 @@ namespace GestaoAluguelWeb.Mappers
 {
     public class LocacaoProfile : Profile
     {
-        public LocacaoProfile() 
+        public LocacaoProfile()
         {
-            CreateMap<LocacaoModel, Locacao>().ReverseMap();
+            // 1. Mapeamento de Pessoa (Model <-> Entity)
+            CreateMap<PessoaModel, Pessoa>().ReverseMap();
+
+            // 2. IDA: Da Tela (Model) para o Banco (Entity)
+            CreateMap<LocacaoModel, Locacao>()
+                // Mapeia o ID do Model (IdInquilino) para o ID do Banco (IdPessoa)
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.IdInquilino));
+            // REMOVIDO: .ForMember(dest => dest.Pessoa...) pois a propriedade não existe
+
+            // 3. VOLTA: Do Banco (Entity) para a Tela (Model)
+            CreateMap<Locacao, LocacaoModel>()
+                // Mapeia o ID do Banco (IdPessoa) para o ID do Model (IdInquilino)
+                .ForMember(dest => dest.IdInquilino, opt => opt.MapFrom(src => src.Id));
+            // REMOVIDO: .ForMember(dest => dest.Inquilino...) pois src.Pessoa não existe
         }
     }
 }
